670fb368622eb678a59bd292faefa8b631fe9675
xcyxcy1992 <xcyxcy1992@hotmail.com>
2016-01-25 14:26:22 +0800

    左滑页面

--- Stamp/Stamp/LeftViewController.m
+++ Stamp/Stamp/LeftViewController.m
@@ -8,7 +8,13 @@
 
 #import "LeftViewController.h"
 
-@interface LeftViewController ()
+#define SCREEN_W [UIScreen mainScreen].bounds.size.width
+#define SCREEN_H [UIScreen mainScreen].bounds.size.height
+
+@interface LeftViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSArray * tableViewArr;
+@property (nonatomic, strong) UITableView * tableView;
 
 @end
 
@@ -16,13 +22,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
     
-    UIImageView * imageV = [[UIImageView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.tableViewArr = @[@"最新公告", @"二维码", @"关于我们"];
     
-    imageV.image = [UIImage imageNamed:@"slide_left_bg"];
+//    UIImageView * imageV = [[UIImageView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+//    
+//    imageV.image = [UIImage imageNamed:@"slide_left_bg"];
+//    
+//    [self.view addSubview:imageV];
+    [self createTableView];
+}
+
+- (void)createTableView
+{
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, SCREEN_H) style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.scrollEnabled = NO;
     
-    [self.view addSubview:imageV];
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark -- tableView代理
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.tableViewArr.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"leftSlideCell"];
+    
+    if(!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleDefault) reuseIdentifier:@"leftSlideCell"];
+    }
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.text = self.tableViewArr[indexPath.row];
+    return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, 240)];
+    UIImageView * imageView = [[UIImageView alloc] initWithFrame:view.frame];
+    imageView.image = [UIImage imageNamed:@"left_background.jpg"];
+    [view addSubview:imageView];
+    return view;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 240;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,14 +87,15 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
+
+
+
+
+
+
+
+
+
